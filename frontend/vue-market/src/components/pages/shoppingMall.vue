f2400778eb06fc15588b3e0f4c0280b4f8095a25
ITvision <18877122131@163.com>
2021-11-23 00:49:19 +0800

    1. add base book Principle of computer composition 2. init python flask program 3. flask programe current progress: finished register template

---
+++ frontend/vue-market/src/components/pages/shoppingMall.vue
@@ -0,0 +1,52 @@
+/* eslint-disable global-require */
+<template>
+  <div>
+    <div class="search-bar">
+      <van-row class="test-row">
+        <van-col span="3">
+          <img :src="locationIcon" width="80%" alt="" class="location-icon">
+        </van-col>
+        <van-col span="16">
+          <input type="text" class="search-input">
+        </van-col>
+        <van-col span="5">
+          <van-button size="mini">查找</van-button>
+        </van-col>
+      </van-row>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'hello',
+      // eslint-disable-next-line global-require
+      locationIcon: require('../../assets/images/location.png'),
+    };
+  },
+};
+</script>
+
+<style scoped>
+.search-bar {
+  height: 2.2rem;
+  background-color: #e5017d;
+  line-height: 2.2rem;
+}
+.location-icon {
+  padding-top: .2rem;
+  padding-left: .3rem;
+}
+.search-input {
+  width: 100%;
+  height: 1.3rem;
+  border-top: 0px;
+  border-left: 0px;
+  border-right: 0px;
+  border-bottom: 1px solid #fff !important;
+  background-color: #e5017d;
+  color: #fff;
+}
+</style>
